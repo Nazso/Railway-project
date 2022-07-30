d34cfad3f1d5b7b2d7af22719efd643b8a51e55f
Nazso <zsolt.nagy.1976@gmail.com>
2022-07-30 17:18:19 +0200

    under dev

--- Locomotives/src/app/app.module.ts
+++ Locomotives/src/app/app.module.ts
@@ -1,18 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+//Layout
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './page/home/home.component';
+import { DieselComponent } from './page/diesel/diesel.component';
+import { ElectricComponent } from './page/electric/electric.component';
+import { CommentsComponent } from './page/comments/comments.component';
+import { BuyItemsComponent } from './page/buy-items/buy-items.component';
+import { AdminComponent } from './page/admin/admin.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    BuyItemsComponent,
+    CommentsComponent,
+    DieselComponent,
+    ElectricComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Locomotives/src/app/page/buy-items/buy-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-items',
+  templateUrl: './buy-items.component.html',
+  styleUrls: ['./buy-items.component.scss']
+})
+export class BuyItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
