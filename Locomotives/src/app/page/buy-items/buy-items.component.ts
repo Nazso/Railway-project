a37d197ac5f2a06dfc9ac46a81e1f0e6a835a29a
Nazso <zsolt.nagy.1976@gmail.com>
2023-04-06 10:37:58 +0200

    developed logic

--- Locomotives/src/app/page/buy-items/buy-items.component.ts
+++ Locomotives/src/app/page/buy-items/buy-items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-buy-items',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyItemsComponent implements OnInit {
 
+  public buyItemForm: FormGroup = new FormGroup({
+    
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buyItem() {
+
+  }
+
+  canselItem() {
+
   }
 
 }
