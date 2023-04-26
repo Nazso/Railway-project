dccb133dbf36f6c08e5ce47c8dd6c139146f7671
Nazso <zsolt.nagy.1976@gmail.com>
2023-04-26 11:09:19 +0200

    developed guards

--- Locomotives/src/app/service/buyitem-deactivate-guard.service.ts
+++ Locomotives/src/app/service/buyitem-deactivate-guard.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { BuyItemsComponent } from '../page/buy-items/buy-items.component';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BuyitemDeactivateGuardService {
+export class BuyitemDeactivateGuardService implements CanDeactivate<BuyItemsComponent> {
 
   constructor() { }
+
+  canDeactivate(component: BuyItemsComponent, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot, nextState?: RouterStateSnapshot | undefined): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    
+    if(component.buyItemForm.dirty && component.buyItemForm.value) {
+
+      return confirm('Would you like to live this page?') ? true : false;
+
+    }
+
+    return true;
+  }
+
 }
--- Locomotives/src/app/service/comment-deactivate-guard.service.ts
+++ Locomotives/src/app/service/comment-deactivate-guard.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CommentsComponent } from '../page/comments/comments.component';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CommentDeactivateGuardService {
+export class CommentDeactivateGuardService implements CanDeactivate<CommentsComponent> {
 
   constructor() { }
+
+  canDeactivate(component: CommentsComponent, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot, nextState?: RouterStateSnapshot | undefined): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    
+    if(component.commentForm.dirty && component.commentForm.value) {
+
+      return confirm('Would you like to live this page?') ? true : false;
+
+    }
+
+    return true;
+  }
 }
